9b5810b9f4bceb098dd0076acf9c2016396aad31
TLT <veriantyronelouis@gmail.com>
2026-07-22 23:12:37 +0800

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+    let C = (num - 32) * (5/9);
+    if (Number.isInteger(C)) return C;
+    else return +C.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+    let F = (num * (9/5)) + 32;
+    if (Number.isInteger(F)) return F;
+    else return +F.toFixed(1);
+
 };
 
 // Do not edit below this line
