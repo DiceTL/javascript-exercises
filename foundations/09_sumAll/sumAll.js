0ea4b694da0a88b601a53124582613b3cb689df2
TLT <veriantyronelouis@gmail.com>
2026-07-22 22:21:19 +0800

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        let start = (a < b) ? a : b;
+        let end = (a > b) ? a : b;
+        for (; start <= end; start++) {
+            sum += start;
+        }
+        return sum;
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
