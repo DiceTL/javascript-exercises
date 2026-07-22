9a04d64809f587181db75fd71464626b92cd65e7
TLT <veriantyronelouis@gmail.com>
2026-07-22 22:02:33 +0800

    Finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    return arr.filter((item, index) => {
+        for (removeItem of theArgs) {
+            if (item === removeItem) {
+                return;
+            }
+        }
+        return item;
+    });  
 };
 
 // Do not edit below this line
