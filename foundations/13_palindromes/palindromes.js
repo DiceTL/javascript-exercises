2e1d23134d0db0073f2033e2ce409ff54792f5fa
TLT <veriantyronelouis@gmail.com>
2026-07-30 15:17:30 +0800

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+/*
+    GET str
+    REMOVE spaces and punctuations of str
+    INIT reverseStr as the reverse of str
+    COMPARE if str and reverseStr is equal if yes, return true, else return false;
+*/
 
+const palindromes = function (str) {
+    const unneededChars = [".", "!", "?", ",", " "];
+    let newStr = str;
+    for (char of str) {
+        if (unneededChars.includes(char)) newStr = newStr.replace(char, "");
+    }
+
+    newStr = newStr.toLowerCase();
+    let reversedStr = newStr
+                        .split("")
+                        .reverse()
+                        .join("")
+                        .toLowerCase();
+    
+    console.log(reversedStr);
+    console.log(newStr);
+    
+    return (newStr === reversedStr);
 };
 
 // Do not edit below this line
