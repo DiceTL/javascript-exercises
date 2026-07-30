69560292b46cec476ccb4c9145719f9fe7b9a32c
TLT <veriantyronelouis@gmail.com>
2026-07-30 15:44:11 +0800

    Finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num < 0) return "OOPS";
+    let x = 0;
+    let y = 1;
 
+    // Holder of Fibonacci
+    let arr = []
+    
+    for (let i = 0; i < num; i++) {
+        arr.push(x);
+        
+        let temp = y;
+        y += x;
+        x = temp;
+    }
+    
+    return x;
 };
 
 // Do not edit below this line
